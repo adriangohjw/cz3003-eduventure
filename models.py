46b1ea769ac4bf61d4bd1083496f4e4680e780e0
adriangohjw <adriangohjw@gmail.com>
2020-03-12 04:42:42 +0800

    added flask_migrate to models

--- models.py
+++ models.py
@@ -3,7 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from config import Config
 
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.URI
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
