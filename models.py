33409ce17cfc5d44a73229b1569057a58d3bf4bf
adriangohjw <adriangohjw@gmail.com>
2020-03-12 04:59:59 +0800

    added User class to models.py

--- models.py
+++ models.py
@@ -16,5 +16,13 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    encrypted_password = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
 if __name__ == '__main__':
     manager.run()
