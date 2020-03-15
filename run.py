9ae2b876cedfdadaa57c35a3918534e14ed5a9ff
adriangohjw <adriangohjw@gmail.com>
2020-03-16 00:23:41 +0800

    restructure app.py and run.py

---
+++ run.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from config import Config
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = Config.URI
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+
+    from models import db 
+    db.init_app(app)
+
+    from services.core.app import user_bp
+    app.register_blueprint(user_bp, url_prefix='/users')
+
+    app.run(port=5000, debug=True)
--- services/qiuz/app.py
+++ services/qiuz/app.py
@@ -3,19 +3,8 @@ from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.URI
-api = Api(app)
-db = SQLAlchemy(app)
-
 class HelloWorld(Resource):
     def get(self):
         return jsonify(
             about = "Hello world!"
         )
-
-api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
