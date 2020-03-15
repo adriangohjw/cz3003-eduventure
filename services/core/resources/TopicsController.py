ff5340586a7256c5037d845c6eb10d940365144a
adriangohjw <adriangohjw@gmail.com>
2020-03-16 06:54:09 +0800

    added to TopicsController: RESTful GET + getTopic()

--- services/core/resources/TopicsController.py
+++ services/core/resources/TopicsController.py
@@ -12,13 +12,44 @@ def create_topic(name):
     else:
         return False
 
-def is_topic(name):
-    return bool(Topic.query.filter_by(name=name).first())
+def is_topic(col, value):
+    if (col == 'name'):
+        return bool(Topic.query.filter_by(name=value).first())
+    elif (col == 'id'):
+        return bool(Topic.query.filter_by(id=value).first())
+
+def getTopic(col, value):
+    if (is_topic(col=col, value=value)):
+        if (col == 'name'):
+            return Topic.query.filter_by(name=value).first()
+        elif (col == 'id'):
+            return Topic.query.filter_by(id=value).first()
+    else:
+        return False
 
 class TopicAPI(Resource):
+    def get(self):
+        id = request.args.get('id')
+        topic = getTopic(col='id', value=id)
+        if (topic):
+            print(len(topic.lessons))
+            return make_response(
+                jsonify(
+                    message = "Topic found",
+                    count_lessons = len(topic.lessons),
+                    lessons = topic.lessons
+                ), 200
+            )
+        else:
+            return make_response(
+                jsonify(
+                    message = "Topic does not exist"
+                ), 404
+            )
+        
     def post(self):
         name = request.args.get('name')
-        if (is_topic(name)):
+        if (is_topic('name', name)):
             return make_response(
                 jsonify(
                     message = "Topic already exist"
