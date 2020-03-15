6928fdefc8d58119a6e6b3e5e1bdb9c923b9ea58
adriangohjw <adriangohjw@gmail.com>
2020-03-16 02:41:23 +0800

    add to StaffsController: is_staff(), GET API

--- services/core/resources/StaffsController.py
+++ services/core/resources/StaffsController.py
@@ -9,7 +9,29 @@ import bcrypt
 
 from .UsersController import is_existing, create_user
 
+def is_staff(email):
+    user = User.query.filter_by(email=email).first()
+    if (user is not None):
+        return bool(Staff.query.filter_by(id=user.id).first())
+    else:
+        return False
+
 class StaffAPI(Resource):
+    def get(self):
+        email = request.args.get('email')
+        if (is_staff(email)):
+            return make_response(
+                jsonify(
+                    message = "User is staff"
+                ), 200
+            )
+        else:
+            return make_response(
+                jsonify(
+                    message = "User is not staff / does not exist"
+                ), 404
+            )
+
     def post(self):
         email = request.args.get('email')
         if (is_existing(email)):
