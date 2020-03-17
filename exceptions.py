797d41a76dac775fca53d2f888ceb1a7fe59ccf7
adriangohjw <adriangohjw@gmail.com>
2020-03-17 18:33:16 +0800

    added ErrorWithCode custom exception

---
+++ exceptions.py
@@ -0,0 +1,6 @@
+class ErrorWithCode(Exception):
+    def __init__(self, status_code, message):
+        self.status_code = status_code
+        self.message = message
+    def __str__(self):
+        return repr(self.status_code)
