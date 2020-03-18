9bfc649113458d1e36449228bede170576a94772
adriangohjw <adriangohjw@gmail.com>
2020-03-19 02:02:29 +0800

    add RsQuizQuestionContainDAO.py

---
+++ services/quiz/dao/RsQuizQuestionContainDAO.py
@@ -0,0 +1,14 @@
+from models import db, Rs_quiz_question_contain
+
+def rsQuizQuestionContainCreate(rs):
+    db.session.add(rs)
+    try:
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def rsQuizQuestionContainRead(quiz_id, question_id):
+    return Rs_quiz_question_contain.query.\
+        filter_by(quiz_id=quiz_id).filter_by(question_id=question_id).first()
