079e85f4711b376a1e49a07f763d15c28607e82b
adriangohjw <adriangohjw@gmail.com>
2020-03-19 02:01:51 +0800

    add rs_quiz_question_contains_contracts

---
+++ services/quiz/contracts/rs_quiz_question_contains_contracts.py
@@ -0,0 +1,25 @@
+from flask import request
+
+from .quizzes_contracts import validate_id as validate_quiz_id
+from .questions_contracts import validate_id as validate_question_id
+
+def questionMngReadContract(request):    
+    quiz_id = request.args.get('quiz_id')
+
+    validate_quiz_id(quiz_id)
+
+    return {
+        'quiz_id': quiz_id
+    }
+
+def questionMngCreateContract(request):
+    quiz_id = request.args.get('quiz_id')
+    question_id = request.args.get('question_id')
+
+    validate_quiz_id(quiz_id)
+    validate_question_id(question_id)
+
+    return {
+        'quiz_id': quiz_id,
+        'question_id': question_id
+    }
