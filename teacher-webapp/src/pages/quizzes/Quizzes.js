3587cfe4b9e77588c6137d374dc0c43eb800fe21
phangjunyu <phangjunyu@gmail.com>
2020-03-18 10:18:36 +0800

    disabled quizzes tab

--- teacher-webapp/src/pages/quizzes/Quizzes.js
+++ teacher-webapp/src/pages/quizzes/Quizzes.js
@@ -1,222 +1,221 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import clsx from "clsx";
-import { lighten, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Switch from "@material-ui/core/Switch";
-import DeleteIcon from "@material-ui/icons/Delete";
-import FilterListIcon from "@material-ui/icons/FilterList";
+// import React, { useState } from "react";
+// import PropTypes from "prop-types";
+// import clsx from "clsx";
+// import { lighten, makeStyles } from "@material-ui/core/styles";
+// import Table from "@material-ui/core/Table";
+// import TableBody from "@material-ui/core/TableBody";
+// import TableCell from "@material-ui/core/TableCell";
+// import TableContainer from "@material-ui/core/TableContainer";
+// import TableHead from "@material-ui/core/TableHead";
+// import TablePagination from "@material-ui/core/TablePagination";
+// import TableRow from "@material-ui/core/TableRow";
+// import TableSortLabel from "@material-ui/core/TableSortLabel";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Paper from "@material-ui/core/Paper";
+// import Checkbox from "@material-ui/core/Checkbox";
+// import IconButton from "@material-ui/core/IconButton";
+// import Tooltip from "@material-ui/core/Tooltip";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Switch from "@material-ui/core/Switch";
+// import DeleteIcon from "@material-ui/icons/Delete";
+// import FilterListIcon from "@material-ui/icons/FilterList";
 
-import { useTheme } from "@material-ui/styles";
+// import { useTheme } from "@material-ui/styles";
 
-// styles
-import useStyles from "./styles";
+// // styles
+// import useStyles from "./styles";
 
-// components
-import mockdata from "./mockdata";
-import PageTitle from "../../components/PageTitle";
-import { Typography } from "../../components/Wrappers";
+// // components
+// import mockdata from "./mockdata";
+// import PageTitle from "../../components/PageTitle";
+// import { Typography } from "../../components/Wrappers";
 
-function EnhancedTableHead(props) {
-  const {
-    classes,
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-  } = props;
+// function EnhancedTableHead(props) {
+//   const {
+//     classes,
+//     onSelectAllClick,
+//     order,
+//     orderBy,
+//     numSelected,
+//     rowCount,
+//   } = props;
 
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{ "aria-label": "select all quizzes" }}
-          />
-        </TableCell>
-        {headCells.map(headCell => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "default"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          ></TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
+//   return (
+//     <TableHead>
+//       <TableRow>
+//         <TableCell padding="checkbox">
+//           <Checkbox
+//             indeterminate={numSelected > 0 && numSelected < rowCount}
+//             checked={rowCount > 0 && numSelected === rowCount}
+//             onChange={onSelectAllClick}
+//             inputProps={{ "aria-label": "select all quizzes" }}
+//           />
+//         </TableCell>
+//         {headCells.map(headCell => (
+//           <TableCell
+//             key={headCell.id}
+//             align={headCell.numeric ? "right" : "left"}
+//             padding={headCell.disablePadding ? "none" : "default"}
+//             sortDirection={orderBy === headCell.id ? order : false}
+//           ></TableCell>
+//         ))}
+//       </TableRow>
+//     </TableHead>
+//   );
+// }
 
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
+// EnhancedTableHead.propTypes = {
+//   classes: PropTypes.object.isRequired,
+//   numSelected: PropTypes.number.isRequired,
+//   onSelectAllClick: PropTypes.func.isRequired,
+//   order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+//   orderBy: PropTypes.string.isRequired,
+//   rowCount: PropTypes.number.isRequired,
+// };
 
-const useToolbarStyles = makeStyles(theme => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === "light"
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: "1 1 100%",
-  },
-}));
+// const useToolbarStyles = makeStyles(theme => ({
+//   root: {
+//     paddingLeft: theme.spacing(2),
+//     paddingRight: theme.spacing(1),
+//   },
+//   highlight:
+//     theme.palette.type === "light"
+//       ? {
+//           color: theme.palette.secondary.main,
+//           backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+//         }
+//       : {
+//           color: theme.palette.text.primary,
+//           backgroundColor: theme.palette.secondary.dark,
+//         },
+//   title: {
+//     flex: "1 1 100%",
+//   },
+// }));
 
-const EnhancedTableToolbar = props => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
+// const EnhancedTableToolbar = props => {
+//   const classes = useToolbarStyles();
+//   const { numSelected } = props;
 
-  return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          className={classes.title}
-          color="inherit"
-          variant="subtitle1"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography className={classes.title} variant="h6" id="tableTitle">
-          Nutrition
-        </Typography>
-      )}
+//   return (
+//     <Toolbar
+//       className={clsx(classes.root, {
+//         [classes.highlight]: numSelected > 0,
+//       })}
+//     >
+//       {numSelected > 0 ? (
+//         <Typography
+//           className={classes.title}
+//           color="inherit"
+//           variant="subtitle1"
+//         >
+//           {numSelected} selected
+//         </Typography>
+//       ) : (
+//         <Typography className={classes.title} variant="h6" id="tableTitle">
+//           Nutrition
+//         </Typography>
+//       )}
 
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton aria-label="filter list">
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
+//       {numSelected > 0 ? (
+//         <Tooltip title="Delete">
+//           <IconButton aria-label="delete">
+//             <DeleteIcon />
+//           </IconButton>
+//         </Tooltip>
+//       ) : (
+//         <Tooltip title="Filter list">
+//           <IconButton aria-label="filter list">
+//             <FilterListIcon />
+//           </IconButton>
+//         </Tooltip>
+//       )}
+//     </Toolbar>
+//   );
+// };
 
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
+// EnhancedTableToolbar.propTypes = {
+//   numSelected: PropTypes.number.isRequired,
+// };
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100%",
-  },
-  paper: {
-    width: "100%",
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: "rect(0 0 0 0)",
-    height: 1,
-    margin: -1,
-    overflow: "hidden",
-    padding: 0,
-    position: "absolute",
-    top: 20,
-    width: 1,
-  },
-}));
+// // const useStyles = makeStyles(theme => ({
+// //   root: {
+// //     width: "100%",
+// //   },
+// //   paper: {
+// //     width: "100%",
+// //     marginBottom: theme.spacing(2),
+// //   },
+// //   table: {
+// //     minWidth: 750,
+// //   },
+// //   visuallyHidden: {
+// //     border: 0,
+// //     clip: "rect(0 0 0 0)",
+// //     height: 1,
+// //     margin: -1,
+// //     overflow: "hidden",
+// //     padding: 0,
+// //     position: "absolute",
+// //     top: 20,
+// //     width: 1,
+// //   },
+// // }));
 
-export default function Quizzes(props) {
-  var classes = useStyles();
-  var theme = useTheme();
+// export default function Quizzes(props) {
+//   var classes = useStyles();
+//   var theme = useTheme();
 
-  // local
-  var [checked, setChecked] = useState([0]);
+//   // local
+//   var [checked, setChecked] = useState([0]);
 
-  const handleToggle = value => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
+//   const handleToggle = value => () => {
+//     const currentIndex = checked.indexOf(value);
+//     const newChecked = [...checked];
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
+//     if (currentIndex === -1) {
+//       newChecked.push(value);
+//     } else {
+//       newChecked.splice(currentIndex, 1);
+//     }
 
-    setChecked(newChecked);
-  };
+//     setChecked(newChecked);
+//   };
 
-  return (
-    <>
-      <PageTitle title="Quizzes" button="Add New Quiz" />
-      <List className={classes.root}>
-        {[0, 1, 2, 3].map(value => {
-          const labelId = `checkbox-list-label-${value}`;
+//   return (
+//     <>
+//       <PageTitle title="Quizzes" button="Add New Quiz" />
+//       <List className={classes.root}>
+//         {[0, 1, 2, 3].map(value => {
+//           const labelId = `checkbox-list-label-${value}`;
 
-          return (
-            <ListItem
-              key={value}
-              role={undefined}
-              dense
-              button
-              onClick={handleToggle(value)}
-            >
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-              <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="comments">
-                  <CommentIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          );
-        })}
-      </List>
-    </>
-  );
-}
+//           return (
+//             <ListItem
+//               key={value}
+//               role={undefined}
+//               dense
+//               button
+//               onClick={handleToggle(value)}
+//             >
+//               <ListItemIcon>
+//                 <Checkbox
+//                   edge="start"
+//                   checked={checked.indexOf(value) !== -1}
+//                   tabIndex={-1}
+//                   disableRipple
+//                   inputProps={{ "aria-labelledby": labelId }}
+//                 />
+//               </ListItemIcon>
+//               <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+//               <ListItemSecondaryAction>
+//                 <IconButton edge="end" aria-label="comments">
+//                   <CommentIcon />
+//                 </IconButton>
+//               </ListItemSecondaryAction>
+//             </ListItem>
+//           );
+//         })}
+//       </List>
+//     </>
+//   );
+// }
