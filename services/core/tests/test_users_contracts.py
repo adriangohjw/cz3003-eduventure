ed8c32eb834db8b50148c5ffbee09ba34c69b020
adriangohjw <adriangohjw@gmail.com>
2020-03-20 04:51:50 +0800

    add test_users_contracts

---
+++ services/core/tests/test_users_contracts.py
@@ -0,0 +1,39 @@
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+import requests
+import unittest
+
+from contracts.users_contracts import validate_email, validate_password
+
+class Test_user_contracts(unittest.TestCase):
+
+    def test_validate_email(self):
+
+        with self.assertRaises(TypeError):
+
+            validate_email(None)
+
+        with self.assertRaises(ValueError):
+
+            validate_email("")
+            
+            validate_email("@gmail.com")
+            validate_email("johndoegmail.com")
+            validate_email("johndoe@gmailcom")
+            
+    def test_validate_password(self):
+
+        with self.assertRaises(TypeError):
+
+            validate_password(None)
+
+        with self.assertRaises(ValueError):
+
+            validate_password("")
+        
+
+if __name__ == '__main__':
+    unittest.main()
+    
