446d11ea5d802d280f874bc251560f08304ed889
adriangohjw <adriangohjw@gmail.com>
2020-03-20 05:42:06 +0800

    add print statements to improve visibility of test

--- services/core/contracts/tests/test_users_contracts.py
+++ services/core/contracts/tests/test_users_contracts.py
@@ -10,6 +10,16 @@ from services.core.contracts.users_contracts import validate_email, validate_pas
 
 class Test_user_contracts(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print("\n\n{}: starting test...".format(path.basename(__file__)))
+
+    def setUp(self):
+        print("\n>>>>> {}: starting test...".format(self._testMethodName))
+
+    def tearDown(self):
+        print(">>>>> {}: ending test...".format(self._testMethodName))
+
     def test_validate_email(self):
 
         with self.assertRaises(TypeError):
@@ -33,7 +43,10 @@ class Test_user_contracts(unittest.TestCase):
         with self.assertRaises(ValueError):
 
             validate_password("")
-        
+
+    @classmethod
+    def tearDownClass(cls):
+        print("\n{}: ending test...".format(path.basename(__file__)))
 
 if __name__ == '__main__':
     unittest.main()
