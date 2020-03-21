7d612038df2e7a0935c8cd2d49895d6fddbe865c
adriangohjw <adriangohjw@gmail.com>
2020-03-21 18:53:05 +0800

    separate db init and config from models.py

---
+++ manage.py
@@ -0,0 +1,16 @@
+from models import db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from run import create_app
+
+app = create_app()
+db.init_app(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
+    
--- models.py
+++ models.py
@@ -1,20 +1,7 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from config import Config
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from sqlalchemy.ext.hybrid import hybrid_property
 
-from run import create_app
-app = create_app()
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
