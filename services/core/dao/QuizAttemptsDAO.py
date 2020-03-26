8174fd5441ab98815dc88cfc08821e0c4a3b59f6
adriangohjw <adriangohjw@gmail.com>
2020-03-27 01:23:50 +0800

    add quizAttemptLeaderboardRead in QuizAttemptsDAO

--- services/core/dao/QuizAttemptsDAO.py
+++ services/core/dao/QuizAttemptsDAO.py
@@ -1,4 +1,5 @@
-from models import db, QuizAttempt
+from models import db, QuizAttempt, Student
+from sqlalchemy import func
 
 def quizAttemptCreate(quizAttempt):
     db.session.add(quizAttempt)
@@ -12,3 +13,17 @@ def quizAttemptCreate(quizAttempt):
 def quizAttemptListRead(student_id, quiz_id):
     return QuizAttempt.query.\
         filter_by(student_id=student_id).filter_by(quiz_id=quiz_id).all()
+
+def quizAttemptLeaderboardRead():
+    return db.session.query(
+        Student.id.label("student_id"), 
+        Student.name.label("student_name"),
+        Student.email.label("student_email"),
+        func.sum(QuizAttempt.score).label("total_score")
+    ).join(
+        Student.quiz_attempts
+    ).group_by(
+        Student.id,
+        Student.name,
+        Student.email
+    ).all()
