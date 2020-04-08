2738a62efb4c22faebaf11087b86ab581a9d95df
adriangohjw <adriangohjw@gmail.com>
2020-04-09 05:49:10 +0800

    services.contracts: add type check

--- services/core/contracts/quizAttempts_contracts.py
+++ services/core/contracts/quizAttempts_contracts.py
@@ -9,6 +9,10 @@ def validate_student_id(student_id):
     if not student_id: 
         raise ValueError("student_id is empty")
 
+    # check if type is integer
+    if not isinstance(student_id, int):
+        raise TypeError("student_id is not an integer")
+
 def validate_quiz_id(quiz_id):
     # if no 'quiz_id' found in params
     if (quiz_id is None):
@@ -17,6 +21,10 @@ def validate_quiz_id(quiz_id):
     # if quiz_id params is empty
     if not quiz_id: 
         raise ValueError("quiz_id is empty")
+
+    # check if type is integer
+    if not isinstance(quiz_id, int):
+        raise TypeError("quiz_id is not an integer")
 
 def validate_score(score):
     # if no 'score' found in params
@@ -27,13 +35,17 @@ def validate_score(score):
     if not score: 
         raise ValueError("score is empty")
 
+    # check if type is integer
+    if not isinstance(score, int):
+        raise TypeError("score is not an integer")
+
     # ensure score is less than 0
     if int(score) < 0:
         raise ValueError("score must be more than or equal to zero")
 
 def quizAttemptListReadContract(request):    
-    student_id = request.args.get('student_id')
-    quiz_id = request.args.get('quiz_id')
+    student_id = request.args.get('student_id', type=int)
+    quiz_id = request.args.get('quiz_id', type=int)
 
     validate_student_id(student_id)
     validate_quiz_id(quiz_id)
@@ -44,9 +56,9 @@ def quizAttemptListReadContract(request):
     }
 
 def quizAttemptCreateContract(request):
-    student_id = request.args.get('student_id')
-    quiz_id = request.args.get('quiz_id')
-    score = request.args.get('score')
+    student_id = request.args.get('student_id', type=int)
+    quiz_id = request.args.get('quiz_id', type=int)
+    score = request.args.get('score', type=int)
 
     validate_student_id(student_id)
     validate_quiz_id(quiz_id)
