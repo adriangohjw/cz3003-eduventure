74c43f321f4b0e536dc0ac2e0b774969ac7da782
adriangohjw <adriangohjw@gmail.com>
2020-04-09 08:50:35 +0800

    update contracts to accept params('name')

--- services/core/contracts/staffs_contracts.py
+++ services/core/contracts/staffs_contracts.py
@@ -2,6 +2,15 @@ from flask import request
 
 from .users_contracts import validate_email, validate_password
 
+def validate_name(name):
+    # if no 'name' found in params
+    if (name is None):
+        raise TypeError("Request params (name) not found")
+
+    # if name params is empty
+    if not name: 
+        raise ValueError("Name is empty")
+    
 def staffReadContract(request):    
     email = request.args.get('email')
 
@@ -14,11 +23,14 @@ def staffReadContract(request):
 def staffCreateContract(request):
     email = request.args.get('email')
     password = request.args.get('password')
+    name = request.args.get('name')
     
     validate_email(email)
     validate_password(password)
+    validate_name(name)
     
     return {
         'email': email,
-        'password': password
+        'password': password,
+        'name': name
     }
