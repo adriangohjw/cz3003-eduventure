adf50ae303a808b89c78b2ab371661a4c2aa0f52
adriangohjw <adriangohjw@gmail.com>
2020-04-10 22:03:08 +0800

    add Quiz.QuestionManagerAPI_DELETE to DAO

--- services/quiz/dao/RsQuizQuestionContainDAO.py
+++ services/quiz/dao/RsQuizQuestionContainDAO.py
@@ -12,3 +12,13 @@ def rsQuizQuestionContainCreate(rs):
 def rsQuizQuestionContainRead(quiz_id, question_id):
     return Rs_quiz_question_contain.query.\
         filter_by(quiz_id=quiz_id).filter_by(question_id=question_id).first()
+
+def rsQuizQuestionContainDelete(quiz_id, question_id):
+    rs = rsQuizQuestionContainRead(quiz_id, question_id)
+    db.session.delete(rs)
+    try:
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
