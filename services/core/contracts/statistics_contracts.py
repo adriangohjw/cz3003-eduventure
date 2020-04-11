925aa128164da930b10330c81b37d92d09f8fca9
adriangohjw <adriangohjw@gmail.com>
2020-04-12 06:07:08 +0800

    add contract (and unittest) for StudentScore_API (SA4)

--- services/core/contracts/statistics_contracts.py
+++ services/core/contracts/statistics_contracts.py
@@ -1,3 +1,11 @@
 from flask import request
 
 from services.core.contracts.courses_contracts import validate_index as validate_course_index
+
+
+def studentScoreReadContract(student_id):
+    student_id = request.args.get('student_id', type=int)
+
+    return {
+        'student_id': student_id
+    }
--- services/core/contracts/tests/test_statistics_contracts.py
+++ services/core/contracts/tests/test_statistics_contracts.py
@@ -11,13 +11,26 @@ app = create_app()
 app.app_context().push()
 db.init_app(app)
 
-#from services.core.contracts.statistics_contracts import 
+from services.core.contracts.statistics_contracts import \
+    studentScoreReadContract
+
 
 class Test_statistics_contracts(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         print("\n\n{}: starting test...".format(path.basename(__file__)))
+
+
+    def test_studentScoreReadContract(self):
+
+        with app.test_request_context('/?student_id=1', method='GET'):
+            self.assertEqual(
+                studentScoreReadContract(request), 
+                {
+                    'student_id': 1
+                }
+            )
 
 
 if __name__ == '__main__':
