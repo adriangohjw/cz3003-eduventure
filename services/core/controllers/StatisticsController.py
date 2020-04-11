bc38e2b7be570e93264c3d9d80c92d59d06daca1
adriangohjw <adriangohjw@gmail.com>
2020-04-12 05:14:32 +0800

    add controller for LeaderBoard_API (SA3)

--- services/core/controllers/StatisticsController.py
+++ services/core/controllers/StatisticsController.py
@@ -9,7 +9,7 @@ from services.core.contracts.statistics_contracts import \
 """
 
 from services.core.operations.statistics_operations import \
-    statReadOperation, lessonCompletedReadOperation
+    statReadOperation, lessonCompletedReadOperation, leaderboardReadOperation
 
 from exceptions import ErrorWithCode
 
@@ -50,3 +50,22 @@ class Lesson_Completion_API(Resource):
         return make_response(
             jsonify (stat), 200
         )
+
+class LeaderBoard_API(Resource):
+
+    def get(self):
+        # operations
+        try:
+            stat = leaderboardReadOperation()
+        except ErrorWithCode as e:
+            return make_response(
+                jsonify (
+                    error = e.message
+                ), e.status_code
+            )
+        
+        # success case
+        return make_response(
+            jsonify (stat), 200
+        )
+    
