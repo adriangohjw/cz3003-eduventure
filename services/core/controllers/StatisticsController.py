966e7863f5f9bf0299a43a0d99aebed4727e93a0
adriangohjw <adriangohjw@gmail.com>
2020-04-12 03:52:53 +0800

    add controller for Lesson_Completion_API (SA2)

--- services/core/controllers/StatisticsController.py
+++ services/core/controllers/StatisticsController.py
@@ -9,7 +9,7 @@ from services.core.contracts.statistics_contracts import \
 """
 
 from services.core.operations.statistics_operations import \
-    statReadOperation
+    statReadOperation, lessonCompletedReadOperation
 
 from exceptions import ErrorWithCode
 
@@ -20,6 +20,25 @@ class Stats_API(Resource):
         # operations
         try:
             stat = statReadOperation()
+        except ErrorWithCode as e:
+            return make_response(
+                jsonify (
+                    error = e.message
+                ), e.status_code
+            )
+        
+        # success case
+        return make_response(
+            jsonify (stat), 200
+        )
+
+
+class Lesson_Completion_API(Resource):
+
+    def get(self):
+        # operations
+        try:
+            stat = lessonCompletedReadOperation()
         except ErrorWithCode as e:
             return make_response(
                 jsonify (
