efb0778967a66d1cc6811ca022dd49c7eb1d05f6
adriangohjw <adriangohjw@gmail.com>
2020-04-12 07:43:03 +0800

    add controller for CourseScore_API (SA7)

--- services/core/controllers/StatisticsController.py
+++ services/core/controllers/StatisticsController.py
@@ -4,11 +4,11 @@ from flask_restful import Resource
 from flask.helpers import make_response
 
 from services.core.contracts.statistics_contracts import \
-    studentScoreReadContract
+    studentScoreReadContract, courseScoreReadContract
 
 from services.core.operations.statistics_operations import \
     statReadOperation, lessonCompletedReadOperation, leaderboardReadOperation, \
-    studentScoreReadOperation
+    studentScoreReadOperation, courseScoreReadOperation
 
 from exceptions import ErrorWithCode
 
@@ -97,3 +97,35 @@ class StudentScore_API(Resource):
         return make_response(
             jsonify (stat), 200
         )
+
+
+class CourseScore_API(Resource):
+
+    def get(self):
+        # contracts
+        try:
+            s = courseScoreReadContract(request)
+        except Exception as e:
+            return make_response(
+                jsonify (
+                    error = str(e),
+                ), 400
+            )
+
+        # operations
+        try:
+            stat = courseScoreReadOperation(
+                s['course_index']
+            )
+        except ErrorWithCode as e:
+            return make_response(
+                jsonify (
+                    error = e.message
+                ), e.status_code
+            )
+        
+        # success case
+        return make_response(
+            jsonify (stat), 200
+        )
+
