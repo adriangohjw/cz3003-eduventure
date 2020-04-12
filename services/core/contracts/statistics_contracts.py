50bc4b0a0ca7b2bc1aa3603105be91217608b766
adriangohjw <adriangohjw@gmail.com>
2020-04-12 09:29:25 +0800

    add contracts (and unittest) for Activity_API (SA6)

--- services/core/contracts/statistics_contracts.py
+++ services/core/contracts/statistics_contracts.py
@@ -1,6 +1,27 @@
 from flask import request
 
-def studentScoreReadContract(student_id):
+import datetime
+from dateutil.parser import parse
+
+from services.quiz.contracts.quizzes_contracts import \
+    validate_date_start, validate_date_end
+
+
+def validate_id(id):
+    # if no 'id' found in params
+    if (id is None):
+        raise TypeError("Request params (id) not found")
+
+    # if id params is empty
+    if not id: 
+        raise ValueError("id is empty")
+
+    # check if type is integer
+    if not isinstance(id, int):
+        raise TypeError("id is not an integer")
+
+
+def studentScoreReadContract(request):
     student_id = request.args.get('student_id', type=int)
 
     return {
@@ -8,9 +29,25 @@ def studentScoreReadContract(student_id):
     }
 
 
-def courseScoreReadContract(course_index):
+def courseScoreReadContract(request):
     course_index = request.args.get('course_index')
 
     return {
         'course_index': course_index
+    }
+
+
+def activityReadContract(request):
+    date_start = request.args.get('date_start')
+    date_end = request.args.get('date_end')
+    student_id = request.args.get('student_id', type=int)
+
+    validate_date_start(date_start)
+    validate_date_end(date_end)
+    validate_id(student_id)
+
+    return {
+        'date_start': datetime.date(parse(date_start).year, parse(date_start).month, parse(date_start).day),
+        'date_end': datetime.date(parse(date_end).year, parse(date_end).month, parse(date_end).day),
+        'student_id': student_id
     }
--- services/core/contracts/tests/test_statistics_contracts.py
+++ services/core/contracts/tests/test_statistics_contracts.py
@@ -11,8 +11,10 @@ app = create_app()
 app.app_context().push()
 db.init_app(app)
 
+import datetime
 from services.core.contracts.statistics_contracts import \
-    studentScoreReadContract, courseScoreReadContract
+    validate_id, \
+    studentScoreReadContract, courseScoreReadContract, activityReadContract
 
 
 class Test_statistics_contracts(unittest.TestCase):
@@ -42,6 +44,28 @@ class Test_statistics_contracts(unittest.TestCase):
                     'course_index': 'cz1003'
                 }
             )
+
+    
+    def test_activityReadContract(self):
+
+        with app.test_request_context('/?date_start=2020-03-30&date_end=2020-03-31&student_id=1', method='GET'):
+            self.assertEqual(
+                activityReadContract(request), 
+                {
+                    'date_start': datetime.date(2020, 3, 30),
+                    'date_end': datetime.date(2020, 3, 31),
+                    'student_id': 1
+                }
+            )
+
+        with app.test_request_context('/?date_start=2020-03-30&date_end=2020-03-31&student_id=', method='GET'):
+            self.assertRaises(TypeError, activityReadContract, request)
+
+        with app.test_request_context('/?date_start=2020-03-30&date_end=20-03-31&student_id=1', method='GET'):
+            self.assertRaises(ValueError, activityReadContract, request)
+
+        with app.test_request_context('/?date_start=2020-03-30&date_end=2020-03-31', method='GET'):
+            self.assertRaises(TypeError, activityReadContract, request)
 
 
 if __name__ == '__main__':
