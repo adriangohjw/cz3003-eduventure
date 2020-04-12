ce56cb6d159c02260830663988510c12e3073be1
adriangohjw <adriangohjw@gmail.com>
2020-04-13 02:32:23 +0800

    add controller for TopicListAPI

--- services/core/controllers/TopicsController.py
+++ services/core/controllers/TopicsController.py
@@ -4,7 +4,8 @@ from flask_restful import Resource
 from flask.helpers import make_response
 
 from ..contracts.topics_contracts import topicReadContract, topicCreateContract
-from ..operations.topics_operations import topicReadOperation, topicCreateOperation
+from ..operations.topics_operations import \
+    topicReadOperation, topicCreateOperation, topiclistReadOperation
 from exceptions import ErrorWithCode
 
 class TopicAPI(Resource):
@@ -58,4 +59,24 @@ class TopicAPI(Resource):
         # success case
         return make_response(
             jsonify(topic.asdict()), 200
+        )
+
+
+class TopicListAPI(Resource):
+    def get(self):
+        # operations
+        try:
+            topics = topiclistReadOperation()
+        except ErrorWithCode as e:
+            return make_response(
+                jsonify (
+                    error = e.message
+                ), e.status_code
+            )
+        
+        # success case
+        return make_response(
+            jsonify (
+                topics = [topic.asdict() for topic in topics]
+            ), 200
         )
