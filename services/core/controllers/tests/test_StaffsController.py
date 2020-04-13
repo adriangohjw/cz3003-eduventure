02ee708b1a9fce615ce4186a6aa6aed20f780be0
adriangohjw <adriangohjw@gmail.com>
2020-04-13 18:33:52 +0800

    add IT for Staff.CourseManagerAPI

--- services/core/controllers/tests/test_StaffsController.py
+++ services/core/controllers/tests/test_StaffsController.py
@@ -31,7 +31,6 @@ class Test_staffsController(Test_BaseCase):
         # success case
         from models import Staff
         response = self.app.get('/staffs?email=staff_1@gmail.com')
-        print(response.data)
         res = res_to_dict(response)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(res['name'], 'staff_1')
@@ -52,6 +51,47 @@ class Test_staffsController(Test_BaseCase):
         res = res_to_dict(response)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(res['name'], 'staff_2')
+
+
+    def test_staff_CourseManagerAPI_GET(self):
+
+        # invalid params input
+        response = self.app.get('/staffs/courses?email=')
+        self.assertEqual(response.status_code, 400)
+
+        # record not found
+        response = self.app.get('/staffs/courses?user_email=staff_2@gmail.com')
+        self.assertEqual(response.status_code, 404)
+
+        # success case
+        response = self.app.get('/staffs/courses?user_email=staff_1@gmail.com')
+        res = res_to_dict(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res['name'], 'staff_1')
+
+
+    def test_staff_CourseManagerAPI_POST(self):
+
+        # invalid params input
+        response = self.app.post('/staffs/courses?user_email=staff_1@gmail.com')
+        self.assertEqual(response.status_code, 400)
+
+        # dependency record not found
+        response = self.app.post('/staffs/courses?user_email=staff_1@gmail.com&course_index=cz1003')
+        self.assertEqual(response.status_code, 404)
+
+        # existing record found
+        response = self.app.post('/staffs/courses?user_email=staff_1@gmail.com&course_index=cz1005')
+        self.assertEqual(response.status_code, 412)
+
+        # success case
+        from models import Course
+        course_cz1003 = Course('cz1003')
+        db.session.add(course_cz1003)
+        db.session.commit()
+
+        response = self.app.post('/staffs/courses?user_email=staff_1@gmail.com&course_index=cz1003')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
