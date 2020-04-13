763b322bb5d27021a57ec74dbb8d4fdfd78f88a0
adriangohjw <adriangohjw@gmail.com>
2020-04-14 03:22:01 +0800

    add IT for CoursesController

--- services/core/controllers/tests/tests.py
+++ services/core/controllers/tests/tests.py
@@ -4,6 +4,7 @@ sys.path.append(getcwd())
 
 import unittest
 
+from services.core.controllers.tests.test_CoursesController import Test_coursesController
 from services.core.controllers.tests.test_LessonsController import Test_lessonsController
 from services.core.controllers.tests.test_StudentsController import Test_studentsController
 from services.core.controllers.tests.test_StudentsController import Test_studentsController
