72d6b14f1867a652d9462aade2c063d773cff706
adriangohjw <adriangohjw@gmail.com>
2020-04-15 15:04:13 +0800

    add IT for lessons controller

--- services/core/controllers/tests/test_TopicsController.py
+++ services/core/controllers/tests/test_TopicsController.py
@@ -52,6 +52,57 @@ class Test_topicsController(Test_BaseCase):
         self.assertEqual(res['name'], 'topic_3')
 
 
+    def test_TopicAPI_PUT(self):
+
+        # invalid params input
+        response = self.app.put('/topics?id=&name=new_name')
+        self.assertEqual(response.status_code, 400)
+
+        response = self.app.put('/topics?id=1&name=')
+        self.assertEqual(response.status_code, 400)
+
+        # record not found
+        response = self.app.put('/topics?id=3&name=new_name')
+        self.assertEqual(response.status_code, 404)
+
+        # success case
+        response = self.app.put('/topics?id=1&name=new_name')
+        res = res_to_dict(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res['name'], 'new_name')
+
+
+    def test_TopicAPI_DELETE(self):
+
+        # invalid params input
+        response = self.app.delete('/topics?id=')
+        self.assertEqual(response.status_code, 400)
+
+        # record not found
+        response = self.app.delete('/topics?id=3')
+        self.assertEqual(response.status_code, 404)
+
+        # record is a dependency
+        response = self.app.delete('/topics?id=2') 
+        self.assertEqual(response.status_code, 412)
+
+        # success case
+        from models import Lesson 
+        lessons = Lesson.query.filter_by(topic_id=2).all()
+        for lesson in lessons:
+            db.session.delete(lesson)
+        db.session.commit()
+
+        response = self.app.delete('/topics?id=2') 
+        res = res_to_dict(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res['message'], 'Successfully deleted topic')
+
+        from models import Topic
+        topic = Topic.query.filter_by(id=2).first() 
+        self.assertIsNone(topic)
+
+
     def test_TopicListAPI_GET(self):
         
         # success case
