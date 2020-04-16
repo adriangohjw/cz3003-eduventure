62c3f93f2338822c0f2a75e6fbe94dbe9885f0fa
adriangohjw <adriangohjw@gmail.com>
2020-04-17 03:06:28 +0800

    update operations (and UT) for challenges create

--- services/core/operations/tests/test_challenges_operations.py
+++ services/core/operations/tests/test_challenges_operations.py
@@ -13,7 +13,7 @@ app.app_context().push()
 db.init_app(app)
 
 
-from models import User, Student, Staff, Topic, Lesson, Quiz, Challenge
+from models import User, Student, Staff, Topic, Lesson, Quiz, Challenge, Question, QuestionAttempt
 from services.core.operations.users_operations import encrypt
 from services.core.operations.challenges_operations import \
     challengeCreateOperation, challengeReadOperation, challengeUpdateCompletedOperation
@@ -55,6 +55,15 @@ class Test_challenges_operations(unittest.TestCase):
         quiz_2 = Quiz(3, 'quiz_2', True, '2020-03-30', '2020-03-31')
         db.session.add(quiz_2)
 
+        question_1 = Question(1, 1, 'question_1')
+        db.session.add(question_1)
+        question_2 = Question(1, 1, 'question_2')
+        db.session.add(question_2)
+        question_3 = Question(1, 1, 'question_3')
+        db.session.add(question_3)
+        question_4 = Question(1, 1, 'question_4')
+        db.session.add(question_4)
+
         challenge = Challenge(1, 2, 1)
         db.session.add(challenge)
 
@@ -66,27 +75,34 @@ class Test_challenges_operations(unittest.TestCase):
         self.assertIsNotNone(challengeReadOperation(None, None, None, None))
         self.assertIsNotNone(challengeReadOperation(1, 2, 1, None))
         self.assertIsNotNone(challengeReadOperation(1, 2, None, None))
-        self.assertRaises(ErrorWithCode, challengeReadOperation, 1, 2, 2, None)
 
 
     def test_challengeCreateOperation(self):
 
-        self.assertRaises(ErrorWithCode, challengeCreateOperation, 1, 2, 1)
+        # students do not exist
+        self.assertRaises(ErrorWithCode, challengeCreateOperation, 1, 3)
+        self.assertRaises(ErrorWithCode,challengeCreateOperation, 3, 2)
+
+        # do not have enough question attempts
+        self.assertRaises(ErrorWithCode, challengeCreateOperation, 1, 2)
+
+        qa_1 = QuestionAttempt(1, 1, True, 1000)
+        db.session.add(qa_1)
+        qa_2 = QuestionAttempt(1, 2, True, 1000)
+        db.session.add(qa_2)
+        qa_3 = QuestionAttempt(1, 3, True, 1000)
+        db.session.add(qa_3)
+        qa_4 = QuestionAttempt(2, 1, True, 1000)
+        db.session.add(qa_4)
+        qa_5 = QuestionAttempt(2, 2, True, 1000)
+        db.session.add(qa_5)
+        qa_6 = QuestionAttempt(2, 3, True, 1000)
+        db.session.add(qa_6)
+        
+        challengeCreateOperation(1, 2)
 
         self.assertEqual(
-            len(Challenge.query.filter_by(from_student_id=1).filter_by(to_student_id=2).filter_by(quiz_id=2).all()),
-            0
-        )
-
-        challengeCreateOperation(1, 2, 2)
-
-        self.assertEqual(
-            len(Challenge.query.filter_by(from_student_id=1).filter_by(to_student_id=2).filter_by(quiz_id=2).all()),
-            1
-        )
-
-        self.assertEqual(
-            len(Challenge.query.filter_by(from_student_id=1).all()),
+            len(Challenge.query.filter_by(from_student_id=1).filter_by(to_student_id=2).all()),
             2
         )
 
