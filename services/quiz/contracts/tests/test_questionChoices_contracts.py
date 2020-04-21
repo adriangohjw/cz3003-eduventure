322a747b09d0abfe94ea3ed2726404f56099496a
adriangohjw <adriangohjw@gmail.com>
2020-04-21 16:20:28 +0800

    add comments for quiz.contracts

--- services/quiz/contracts/tests/test_questionChoices_contracts.py
+++ services/quiz/contracts/tests/test_questionChoices_contracts.py
@@ -11,48 +11,85 @@ app = create_app()
 app.app_context().push()
 db.init_app(app)
 
-from services.quiz.contracts.questionChoices_contracts import \
-    validate_question_id, validate_questionChoice_id, validate_is_correct, validate_description, \
-    questionChoiceCreateContract, questionChoiceDeleteContract, questionChoiceReadContract, questionChoiceUpdateContract
+from services.quiz.contracts.questionChoices_contracts import (
+    validate_questionChoice_id, 
+    validate_is_correct, 
+    validate_description, 
+    questionChoiceCreateContract, 
+    questionChoiceDeleteContract, 
+    questionChoiceReadContract, 
+    questionChoiceUpdateContract
+)
 
 
+"""
+This is a TestCase object to test the functions in questionChoices_contracts.py
+"""
 class Test_questionChoices_contracts(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         print("\n\n{}: starting test...".format(path.basename(__file__)))
 
-
+    # test the function validate_questionChoice_id
     def test_validate_questionChoice_id(self):
+        print('\r')
 
-        with self.assertRaises(TypeError):
-            validate_questionChoice_id(None)
+        # check if TypeError raised when arg is None type
+        print('--- test if arg is None type')
+        self.assertRaises(TypeError, validate_questionChoice_id, None)
 
-        with self.assertRaises(ValueError):
-            validate_questionChoice_id("")
+        # check if TypeError raised when arg is string type
+        print('--- test if arg is string type')
+        self.assertRaises(TypeError, validate_questionChoice_id, 'testing')
 
+        # check if TypeError raised when arg is float type
+        print('--- test if arg is float type')
+        self.assertRaises(TypeError, validate_questionChoice_id, 1.1)
 
+        # check if TypeError raised when arg is boolean type
+        print('--- test if arg is boolean type')
+        self.assertRaises(TypeError, validate_questionChoice_id, True)
+
+    # test the function validate_description
     def test_validate_description(self):
+        print('\r')
 
-        with self.assertRaises(TypeError):
-            validate_description(None)
+        # check if TypeError raised when arg is None type
+        print('--- test if arg is None type')
+        self.assertRaises(TypeError, validate_description, None)
 
-        with self.assertRaises(ValueError):
-            validate_description("")
+        # check if ValueError raised when arg is empty string
+        print('--- test if arg is empty string')
+        self.assertRaises(ValueError, validate_description, '')
 
-
+    # test the function validate_is_correct
     def test_validate_is_correct(self):
+        print('\r')
 
-        with self.assertRaises(TypeError):
-            validate_is_correct(None)
-            validate_is_correct("test")
-            validate_is_correct(1)
-            validate_is_correct(1.1)
+        # check if TypeError raised when arg is None type
+        print('--- test if arg is None type')
+        self.assertRaises(TypeError, validate_is_correct, None)
 
+        # check if TypeError raised when arg is string type
+        print('--- test if arg is string type')
+        self.assertRaises(TypeError, validate_is_correct, 'testing')
 
+        # check if TypeError raised when arg is integer type
+        print('--- test if arg is float type')
+        self.assertRaises(TypeError, validate_is_correct, 1)
+
+        # check if TypeError raised when arg is float type
+        print('--- test if arg is float type')
+        self.assertRaises(TypeError, validate_is_correct, 1.1)
+
+    # test the function questionChoiceReadContract
     def test_questionChoiceReadContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?question_id=2&questionChoice_id=3', method='GET'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionChoiceReadContract(request), 
                 {
@@ -61,13 +98,20 @@ class Test_questionChoices_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with unacceptable value in params (no value passed into 'questionChoice_id')
+        print('--- test request with unacceptable params value (no value for \'questionChoice_id\')')
         with app.test_request_context('/?question_id=2&questionChoice_id=', method='GET'):
+            # check if TypeError is being raise
             self.assertRaises(TypeError, questionChoiceReadContract, request)
 
-
+    # test the function questionChoiceCreateContract
     def test_questionChoiceCreateContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?question_id=2&description=easy&is_correct=true', method='POST'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionChoiceCreateContract(request), 
                 {
@@ -77,13 +121,20 @@ class Test_questionChoices_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with unacceptable value in params (no value passed into 'is_correct')
+        print('--- test request with unacceptable params value (no value for \'is_correct\')')
         with app.test_request_context('/?question_id=2&description=easy&is_correct=', method='POST'):
+            # check if TypeError is being raise
             self.assertRaises(TypeError, questionChoiceCreateContract, request)
 
-
+    # test the function questionChoiceUpdateContract
     def test_questionChoiceUpdateContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?question_id=2&questionChoice_id=2&description=test_description&is_correct=true', method='PUT'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionChoiceUpdateContract(request),
                 {
@@ -94,16 +145,26 @@ class Test_questionChoices_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with missing params ('is_correct' param missing)
+        print('--- test request with missing params (\'is_correct\')')
         with app.test_request_context('/?question_id=2&questionChoice_id=2', method='PUT'):
+            # check if TypeError is being raise
             self.assertRaises(TypeError, questionChoiceUpdateContract, request)
 
+        # passing request with unacceptable value in params (empty string passed in)
+        print('--- test request with unacceptable params value (empty string for \'description\')')
         with app.test_request_context('/?question_id=2&questionChoice_id=2&description=&is_correct=true', method='PUT'):
+            # check if ValueError is being raise
             self.assertRaises(ValueError, questionChoiceUpdateContract, request)
 
-
+    # test the function questionChoiceDeleteContract
     def test_questionChoiceDeleteContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?question_id=2&questionChoice_id=3', method='DELETE'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionChoiceDeleteContract(request),
                 {
@@ -112,9 +173,12 @@ class Test_questionChoices_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with unacceptable value in params (no value passed into 'questionChoice_id')
+        print('--- test request with unacceptable params value (no value for \'questionChoice_id\')')
         with app.test_request_context('/?question_id=2&questionChoice_id=', method='DELETE'):
+            # check if TypeError is being raise
             self.assertRaises(TypeError, questionChoiceDeleteContract, request)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
--- services/quiz/contracts/tests/test_questions_contracts.py
+++ services/quiz/contracts/tests/test_questions_contracts.py
@@ -11,39 +11,64 @@ app = create_app()
 app.app_context().push()
 db.init_app(app)
 
-from services.quiz.contracts.questions_contracts import \
-    validate_description, validate_id, validate_lesson_id, validate_topic_id, \
-    questionCreateContract, questionDeleteContract, questionReadContract, questionUpdateContract
+from services.quiz.contracts.questions_contracts import (
+    validate_description, 
+    validate_id, 
+    questionCreateContract, 
+    questionDeleteContract, 
+    questionReadContract, 
+    questionUpdateContract
+)
+    
 
-
+"""
+This is a TestCase object to test the functions in questions_contracts.py
+"""
 class Test_questions_contracts(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         print("\n\n{}: starting test...".format(path.basename(__file__)))
 
-
+    # test the function validate_id
     def test_validate_id(self):
+        print('\r')
 
-        with self.assertRaises(TypeError):
-            validate_id(None)
+        # check if TypeError raised when arg is None type
+        print('--- test if arg is None type')
+        self.assertRaises(TypeError, validate_id, None)
 
-        with self.assertRaises(ValueError):
-            validate_id("")
+        # check if TypeError raised when arg is string type
+        print('--- test if arg is string type')
+        self.assertRaises(TypeError, validate_id, 'testing')
 
+        # check if TypeError raised when arg is float type
+        print('--- test if arg is float type')
+        self.assertRaises(TypeError, validate_id, 1.1)
 
+        # check if TypeError raised when arg is boolean type
+        print('--- test if arg is boolean type')
+        self.assertRaises(TypeError, validate_id, True)
+
+    # test the function validate_description
     def test_validate_description(self):
+        print('\r')
 
-        with self.assertRaises(Exception):
-            validate_description(None)
-        
-        with self.assertRaises(ValueError):
-            validate_description("")
+        # check if TypeError raised when arg is None type
+        print('--- test if arg is None type')
+        self.assertRaises(TypeError, validate_description, None)
 
+        # check if ValueError raised when arg is empty string
+        print('--- test if arg is empty string')
+        self.assertRaises(ValueError, validate_description, '')
 
+    # test the function questionReadContract
     def test_questionReadContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?id=2', method='GET'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionReadContract(request), 
                 {
@@ -51,13 +76,20 @@ class Test_questions_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with unacceptable value in params (no value passed into 'id')
+        print('--- test request with unacceptable params value (no value for \'id\')')
         with app.test_request_context('/?id=', method='GET'):
+            # check if TypeError is being raise
             self.assertRaises(TypeError, questionReadContract, request)
 
-
+    # test the function questionCreateContract
     def test_questionCreateContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?topic_id=1&lesson_id=2&description=hard', method='POST'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionCreateContract(request), 
                 {
@@ -67,13 +99,20 @@ class Test_questions_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with unacceptable value in params (empty string passed into 'description')
+        print('--- test request with unacceptable params value (empty string for \'description\')')
         with app.test_request_context('/?topic_id=1&lesson_id=2&description=', method='POST'):
+            # check if ValueError is being raise
             self.assertRaises(ValueError, questionCreateContract, request)
 
-
+    # test the function questionUpdateContract
     def test_questionUpdateContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?id=1&description=hard', method='PUT'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionUpdateContract(request),
                 {
@@ -82,13 +121,20 @@ class Test_questions_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with unacceptable value in params (empty string passed into 'description')
+        print('--- test request with unacceptable params value (empty string for \'description\')')
         with app.test_request_context('/?id=1&description=', method='PUT'):
+            # check if ValueError is being raise
             self.assertRaises(ValueError, questionUpdateContract, request)
 
-
+    # test the function questionDeleteContract
     def test_questionDeleteContract(self):
+        print('\r')
 
+        # passing request with acceptable value
+        print('--- test acceptable request')
         with app.test_request_context('/?id=2', method='DELETE'):
+            # check res returned by func is correct
             self.assertEqual(
                 questionDeleteContract(request),
                 {
@@ -96,9 +142,12 @@ class Test_questions_contracts(unittest.TestCase):
                 }
             )
 
+        # passing request with unacceptable value in params (no value passed into 'id')
+        print('--- test request with unacceptable params value (no value for \'id\')')
         with app.test_request_context('/?id=', method='DELETE'):
+            # check if TyppeError is being raise
             self.assertRaises(TypeError, questionDeleteContract, request)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
